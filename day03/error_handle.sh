449bc284017838781208324374bf35fffba3e1d7
KumarSwarnim19 <kumarswarnim19@gmail.com>
2025-10-07 19:01:24 +0000

    All files

---
+++ day03/error_handle.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+create_directory(){
+	mkdir demo
+}
+if ! create_directory; then 
+	echo "The code is being exited as the directory already exists"
+	exit 1
+fi
+
+echo "this should not work because the code is interrupted"
